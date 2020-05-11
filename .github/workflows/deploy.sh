bb8292340fe10e39dc2c03221da6bed3a50a5081
František Maša <frantisekmasa1@gmail.com>
2020-05-11 17:37:47 +0200

    WIP

--- .github/workflows/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p ~/.ssh
-ssh-keyscan -H www.skauting.cz >> ~/.ssh/known_hosts
-echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_ed25519
-chown www-data:www-data ~/.ssh
-
-eval "$(ssh-agent)"
-ssh-add && phing deploy
